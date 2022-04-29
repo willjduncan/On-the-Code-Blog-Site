2ff915a0c5dabc938ea87bb200b1e8d671197410
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-04-29 09:15:04 -0400

    finish handlebars

--- controllers/index.js
+++ controllers/index.js
@@ -5,8 +5,8 @@ const dashboardRoutes = require('./dashboard-routes.js');
 const apiRoutes = require('./api');
 
 router.use('/api', apiRoutes);
-// router.use('/', homeRoutes);
-// router.use('/dashboard', dashboardRoutes);
+router.use('/', homeRoutes);
+router.use('/dashboard', dashboardRoutes);
 
 router.use((req, res) => {
   res.status(404).end();
