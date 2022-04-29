ddcb97e01039cf113c92fad4202cd0653c860f1a
Will Duncan <willduncan@MBP-8323.attlocal.net>
2022-04-29 10:30:37 -0400

    add public javascript

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -95,7 +95,8 @@ router.post('/', withAuth, (req, res) => {
 router.put('/:id', withAuth, (req, res) => {
     Post.update(
       {
-        title: req.body.title
+        title: req.body.title,
+        content: req.body.content
       },
       {
         where: {
